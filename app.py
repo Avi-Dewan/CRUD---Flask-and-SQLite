7153f7f163338411aa44ca87c37d37e7553c4b42
Avi Dewan <1905120@ugrad.cse.buet.ac.bd>
2023-08-18 17:25:12 +0600

    CRUD on a partipent table

--- app.py
+++ app.py
@@ -1,32 +1,68 @@
-from flask import Flask, render_template, request
-from database import add_participent_to_DB, load_participents
+from flask import Flask, request, make_response, jsonify
+from database import add_participent_to_DB, load_participents, load_participent, update_participant, delete_participant
 
 app = Flask(__name__)
 
 
 @app.route('/')
 
-# @app.route('/home')
 
 
-
-
-@app.route('/join', methods=['GET', 'POST'])
+@app.route('/create', methods=['POST'])
 def join():
 	if request.method == 'POST':
 		data = request.form
 		add_participent_to_DB(data)
-		return "Success"
-	# 	# return render_template("index.html")
-	# else:
-	# 	return render_template('join.html')
+		response = make_response("Successfull created a participent", 200)  # Create a response with "Success" message and status code 200
+		return response
 
 
-@app.route('/participants')
+@app.route('/participants', methods=['GET'])
 def participants():
 	data = load_participents()
 	return data
+	# return jsonify(data)  # Convert the data to a JSON response
 
+
+@app.route('/participant/<name>', methods=['GET'])
+def participant(name):
+    participant_data = load_participent(name)
+    
+    if participant_data is None:
+        return jsonify({"error": "Participant not found"}), 404
+    
+    # Convert the participant data to a dictionary and return as JSON response
+    participant_dict = {
+        "name": participant_data[0],
+        "addr": participant_data[1],
+        "city": participant_data[2],
+        "pin": participant_data[3]
+    }
+    
+    return jsonify(participant_dict)
+
+
+@app.route('/updateParticipant/<name>', methods=['PUT'])
+def update_participant_route(name):
+    updates = request.json  # JSON data in the request body
+
+    if updates is None:
+        return jsonify({"error": "No update data provided"}), 400
+
+    participant_data = load_participent(name)
+    if participant_data is None:
+        return jsonify({"error": "Participant not found"}), 404
+
+    if update_participant(name, updates):
+        return jsonify({"message": "Participant updated successfully"})
+    else:
+        return jsonify({"error": "No valid fields to update"}), 400
+
+
+@app.route('/deleteParticipant/<name>', methods=['DELETE'])
+def delete_participant_route(name):
+    delete_participant(name)
+    return jsonify({"message": f"Participant {name} deleted successfully"})
 
 if __name__ == '__main__':
 	app.run(debug=False)
--- database.py
+++ database.py
@@ -6,6 +6,8 @@ connect.execute(
 	'CREATE TABLE IF NOT EXISTS PARTICIPANTS (name TEXT, email TEXT, city TEXT, country TEXT, phone TEXT)')
 
 
+#  CREATE
+
 def add_participent_to_DB(data):
 
     with sqlite3.connect("database.db") as connection:
@@ -15,11 +17,59 @@ def add_participent_to_DB(data):
                     (data['name'], data['email'], data['city'], data['country'], data['phone']))
         connection.commit()
 
+# READ
 
 def load_participents():
     with sqlite3.connect("database.db") as connection:
         cursor = connection.cursor()
         cursor.execute('SELECT * FROM PARTICIPANTS')
-
+ 
         data = cursor.fetchall()
         return data
+    
+def load_participent(name):
+    with sqlite3.connect("database.db") as connection:
+        cursor = connection.cursor()
+        cursor.execute('SELECT * FROM PARTICIPANTS WHERE name = ?', (name,))
+ 
+        data = cursor.fetchall()
+        if(len(data) == 0):
+            return None
+        
+        return data[0]
+
+
+# UPDATE 
+
+def update_participant(name, updates):
+    with sqlite3.connect("database.db") as connection:
+        cursor = connection.cursor()
+
+        update_query = "UPDATE PARTICIPANTS SET "
+        update_data = []
+        
+        # List of fields that can be updated
+        allowed_fields = ["email", "city", "country", "phone"]
+
+        for field, value in updates.items():
+            if field in allowed_fields:
+                update_query += f"{field} = ?, "
+                update_data.append(value)
+
+        if not update_data:
+            return False  # No valid fields to update
+        
+        update_query = update_query.rstrip(', ') + " WHERE name = ?"
+        update_data.append(name)
+
+        cursor.execute(update_query, update_data)
+        connection.commit()
+        return True
+
+# DELETE
+
+def delete_participant(name):
+    with sqlite3.connect("database.db") as connection:
+        cursor = connection.cursor()
+        cursor.execute('DELETE FROM PARTICIPANTS WHERE name = ?', (name,))
+        connection.commit()
